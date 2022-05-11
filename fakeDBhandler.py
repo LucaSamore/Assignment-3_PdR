10361567feb0f71966efaeb593625f19c74cf60d
Luca Samore <luca.samore@studio.unibo.it>
2022-05-11 11:59:54 +0200

    Add a fake-db handler and start cookies management

---
+++ fakeDBhandler.py
@@ -0,0 +1,29 @@
+from types import SimpleNamespace as Namespace
+import json
+
+class DBHandler:
+    
+    USERS_PATH = 'fake-db/users.json'
+    
+    def __init__(self):
+        self.users = []
+        self.__read_users()
+    
+    def add_user(self, newUser):
+        self.users.append(newUser)
+        self.__save_users()
+    
+    def does_user_exist(self, user):
+        print(user.__dict__)
+        return user.email in map(lambda u: u.email, self.users)
+    
+    def get_users(self):
+        return self.users
+    
+    def __save_users(self):
+        with open(self.USERS_PATH, 'w') as file:
+            json.dump([user.__dict__ for user in self.users], file)
+
+    def __read_users(self):
+        with open(self.USERS_PATH) as file:
+            self.users = json.load(file, object_hook=lambda d: Namespace(**d))
--- server.py
+++ server.py
@@ -1,20 +1,17 @@
 from http.server import SimpleHTTPRequestHandler
+from http import cookies
+from fakeDBhandler import DBHandler
 import cgi
-import json
 import re
 import jwt
 import bcrypt
 
 SECRET='tellnobody'
-users = []
 
-class User:
-    pass
+class User: pass
 
 class ServerHandler(SimpleHTTPRequestHandler):
-
-    def __init__(self, request, client_address, server, directory):
-        users = self._read_users()
+    DB = DBHandler()
     
     def do_POST(self):
         form = cgi.FieldStorage(fp=self.rfile, headers=self.headers, environ={'REQUEST_METHOD':'POST'})
@@ -32,17 +29,28 @@ class ServerHandler(SimpleHTTPRequestHandler):
             if self.__validate_email(email) and self.__validate_password(password):
                 newUser = User()
                 newUser.email = email
-                newUser.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-                # create a new token for the user
-                # save the new user
-                # create a cookie and send it to the user
-                # hit the homepage
+                newUser.password = str(bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()), 'UTF-8')
+                
+                if not self.DB.does_user_exist(newUser):
+                    
+                    # create a new token for the user
+                    token = self.__create_token(newUser)
+                    
+                    # save the new user
+                    self.DB.add_user(newUser)
+                    
+                    # create a cookie and send it to the user
+                    cookie = cookies.SimpleCookie()
+                    cookie['access_token'] = token                
+                    
+                    # hit the homepage
+                else:
+                    self.send_error(409, 'Email already in used')
             else:
                 self.send_error(400, "Don't mess with me...")
         
-        
     def do_GET(self):
-        if not self._is_authorized():
+        if not self.__is_authorized():
             #self.send_error(401, "You're not authenticated bud")
             self.__response("index.html")
             return
@@ -59,14 +67,11 @@ class ServerHandler(SimpleHTTPRequestHandler):
         self.path = f"/pages/{fileName}"
         SimpleHTTPRequestHandler.do_GET(self)
         
-    def __get_token(self, user):
-        return jwt.encode(user, SECRET, algorithm="HS256")
+    def __create_token(self, user):
+        return jwt.encode(user.__dict__, SECRET, algorithm="HS256")
     
     def __decode_token(self, token):
         return jwt.decode(token, SECRET, algorithms=["HS256"])
-    
-    def __does_user_exist(self, user):
-        return user in users
         
     def __validate_email(self, email):
         regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
@@ -75,11 +80,3 @@ class ServerHandler(SimpleHTTPRequestHandler):
     def __validate_password(self, password):
         regex = re.compile(r'[A-Za-z0-9@#$%^&+=]{8,}')
         return re.fullmatch(regex, password)
-    
-    def __save_users(self):
-        with open('users.json', 'w') as file:
-            json.dump(users, file)
-    
-    def __read_users(self):
-        with open('users.json') as file:
-            users = json.load(file)
