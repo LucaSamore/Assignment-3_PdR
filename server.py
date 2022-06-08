266297ca7195e0037cc440c0e5b96e4848cf9fda
Luca Samore <luca.samore@studio.unibo.it>
2022-06-08 17:09:30 +0200

    Add email and password validation

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+import re
+from re import Match, Pattern
 from os import path
 from http.server import SimpleHTTPRequestHandler
 from typing import Optional
@@ -34,3 +36,11 @@ class ServerHandler(SimpleHTTPRequestHandler):
     
     def do_POST(self) -> None:
         pass
+    
+    def validate_email(self, email: str) -> Optional[Match]:
+        regex: Pattern = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+        return re.fullmatch(regex, email)
+
+    def validate_password(self, password: str) -> Optional[Match]:
+        regex: Pattern = re.compile(r'[A-Za-z0-9@#$%^&+=]{8,}')
+        return re.fullmatch(regex, password)
